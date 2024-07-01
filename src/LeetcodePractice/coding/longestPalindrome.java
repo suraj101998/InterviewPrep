2eb3d31fc26d14e2ca63ba7ae534742b7989410c
somnath2015chakraborty@gmail.com <somnath2015chakraborty@gmail.com>
2024-07-01 12:23:59 +0530

    added problems

---
+++ src/LeetcodePractice/coding/longestPalindrome.java
@@ -0,0 +1,37 @@
+/*
+Problem No- 409
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest 
+palindrome that can be built with those letters.
+Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+
+Example 1:
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+Example 2:
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.
+ 
+Constraints:
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+ */
+package LeetcodePractice.coding;
+
+public class longestPalindrome {
+    public int longestPalindromes(String s) {
+        int count[] = new int[128];
+        int n= s.length();
+        int ans = 0;
+        for(int i=0; i<n; ++i){
+            ++count[s.charAt(i)];
+        }
+        for (int v: count){
+            ans += v/2*2;
+        }
+        ans += ans < n ? 1:0;
+        return ans;
+    }    
+}
--- src/LeetcodePractice/coding/mergeSortedList.java
+++ src/LeetcodePractice/coding/mergeSortedList.java
@@ -32,9 +32,15 @@ package LeetcodePractice.coding;
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import LeetcodePractice.coding.Solution.ListNode;
 
 public class mergeSortedList {
+        public class ListNode {
+            int val;
+            ListNode next;
+            ListNode() {}
+            ListNode(int val) { this.val = val; }
+            ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        }
         public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 == null){
             return list2;
